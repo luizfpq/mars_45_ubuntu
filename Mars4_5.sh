542e63d5ed991a8ae44e262467d5bce364d77cc8
LuizQuirino <luiz.quirino@ifsp.edu.br>
2018-09-07 12:54:56 -0300

    Versão inicial do instalador

--- Mars4_5.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar Mars4_5.jar -D javax.accessibility.assistive_technologies=" "
---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# LuizQuirino <luizfpq@gmail.com>
+
+set -eu -o pipefail # fail on error , debug all lines
+
+sudo -n true
+test $? -eq 0 || exit 1 "Você precisa de privilégios de sudo para execução"
+
+#echo A instalação necessita dos seguintes requisitos
+while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
+    default-jre
+    axel
+EOF
+)
+echo
+echo -e "\t Instalando os requisitos"
+echo -e "\n\t Aguarde 5 segundos para prosseguir ..."
+echo -e "\t ou use Ctrl+C para cancelar"
+echo -e "\n"
+sleep 6
+
+
+
+#removemos o diretorio e recriamos apenas por garantia
+sudo rm -rf /opt/Mars
+sudo mkdir /opt/Mars
+#realizamos a configuração de compatibilidade do openjdk
+sudo axel -n 5 https://raw.githubusercontent.com/luizfpq/mars_45_ubuntu/master/configure_openjdk.sh --output=/opt/Mars/configure_openjdk.sh --alternate
+sudo chmod +x /opt/Mars/configure_openjdk.sh
+sudo sh /opt/Mars/configure_openjdk.sh
+#iniciamos o download do Mars
+sudo axel -n 5 https://github.com/luizfpq/mars_45_ubuntu/blob/master/Mars4_5.jar?raw=true --output=/opt/Mars/Mars4_5.jar --alternate
+sudo chmod +x /opt/Mars/Mars4_5.jar
+
+sudo axel -n 5 https://github.com/luizfpq/mars_45_ubuntu/blob/master/Mars4_5.jar?raw=true --output=/opt/Mars/mars.png --alternate
+
+sudo echo "[Desktop Entry]
+Version=1.0
+Name=Unipro UGENE
+Comment=Unipro UGENE is a cross-platform visual environment for DNA and protein sequence analysis.
+Exec=java -jar /opt/Mars/Mars4_5.jar
+Path=/opt/Mars
+Icon=/opt/Mars/mars.png
+Terminal=false
+Type=Application
+Categories=Utility;Development;" >> /usr/share/applications/Mars.desktop
